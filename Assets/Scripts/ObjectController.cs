451010aaa899d5252348d0f9e65528c2e6a90a28
Synneogbjo <91120023+Synneogbjo@users.noreply.github.com>
2021-10-28 10:25:58 +0200

    test

---
+++ Assets/Scripts/ObjectController.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectController : MonoBehaviour
+{
+    public List<GameObject> objects = new List<GameObject>();
+
+    private void FixedUpdate()
+    {
+        foreach (GameObject i in objects)
+        {
+            
+        }
+    }
+}
